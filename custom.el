fef741a93dde51edcd28e0e66dba9c4a0a856c51
Daniel Newman <dwnewman78@gmail.com>
2018-08-28 14:14:25 -0500

    Fixed all loading issues

--- custom.el
+++ custom.el
@@ -3,11 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-export-backends '(ascii beamer html latex md))
+ '(org-export-backends (quote (ascii beamer html latex md)))
  '(package-selected-packages
-   '(flx calfw-gcal calfw-ical calfw-org calfw alert request-deferred deft org-brain ox-reveal org-present wgrep web-mode use-package undo-tree treemacs switch-window pyvenv projectile plantuml-mode pipenv page-break-lines ox-tufte ox-gfm org-plus-contrib org-bullets multiple-cursors move-text minions magithub lsp-ui keycast ivy hungry-delete htmlize helpful gnuplot general expand-region exec-path-from-shell doom-themes company-lsp company-anaconda ccls ag))
+   (quote
+    (gnuplot-mode spacemacs-theme anaconda-mode lsp-mode company magit flx calfw-gcal calfw-ical calfw-org calfw alert request-deferred deft org-brain ox-reveal org-present wgrep web-mode use-package undo-tree treemacs switch-window pyvenv projectile plantuml-mode pipenv page-break-lines ox-tufte ox-gfm org-plus-contrib org-bullets multiple-cursors move-text minions magithub lsp-ui keycast ivy hungry-delete htmlize helpful gnuplot general expand-region exec-path-from-shell doom-themes company-lsp company-anaconda ccls ag)))
  '(safe-local-variable-values
-   '((web-mode-script-padding . 0)
+   (quote
+    ((web-mode-script-padding . 0)
      (web-mode-block-padding . 2)
      (web-mode-style-padding . 2)
      (web-mode-part-padding . 2)
@@ -15,6 +17,7 @@
       (substatement-open . 0)
       (arglist-intro . +)
       (arglist-close . +)))))
+ '(scroll-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
