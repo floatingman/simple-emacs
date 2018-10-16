d31822e750ea0eda3c38fc84b6047b241a714f2b
Daniel Newman <dwnewman78@gmail.com>
2018-10-16 08:05:56 -0500

    Moving towards config files

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;;; early-init.el -*- lexical-binding: t; -*-
+;; Time-stamp: <2018-10-15 20:58:17 daniel.newman>
+
+;; Copyright (C) 2018-2018 Daniel Newman
+;; Author: Daniel Newman <dwnewman78@gmail.com>
+
+(setq package-quickstart t)
---
+++ my-elisp-code/custom-settings.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(deadgrep treemacs-projectile treemacs adaptive-wrap poporg hungry-delete cycle-quotes smart-dash undo-tree move-text expand-region whole-line-or-region org-pomodoro toc-org ox-twbs ox-gfm org-download org-cliplink langtool hydra elisp-def highlight-quoted selected org-sticky-header ob-http ob-rust org-journal osx-trash exec-path-from-shell use-package-ensure-system-package use-package-chords key-chord system-packages use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
